f704489ddf018f57616e54d2d38dd0832db079fd
Daniel Neville <daniel.neville@cs.ox.ac.uk>
2017-02-20 08:07:27 +0000

    Updated to CBMC v5.6

--- cbmc-ubuntu-5.4.sh
+++ cbmc-ubuntu-5.4.sh
@@ -2,7 +2,9 @@ sudo apt-get -y install flex bison
 
 git clone https://github.com/diffblue/cbmc.git
 cd cbmc
-git checkout tags/cbmc-5.4 -b downloaded-cbmc
+git checkout tags/cbmc-5.6 -b cbmc
 cd src
 make minisat2-download
-make clean; make -j$(nproc); make
+make clean;
+make -j 2; 
+make
--- cbmc.rb
+++ cbmc.rb
@@ -5,10 +5,9 @@
 class Cbmc < Formula
   desc "CBMC is a Bounded Model Checker for C and C++ programs. It supports C89, C99, most of C11 and most compiler extensions provided by gcc and Visual Studio." 
   homepage "http://www.cprover.org/cbmc/"
-  url "https://github.com/diffblue/cbmc.git", :tag => "cbmc-5.4"
+  url "https://github.com/diffblue/cbmc.git", :tag => "cbmc-5.6"
 
   option "symex", "Builds path symex."
-
 
   depends_on "bison"
   depends_on "flex"
@@ -21,9 +20,11 @@ class Cbmc < Formula
     Dir.chdir 'src' do
       system "make minisat2-download"
       system "make"
-      bin.install "cbmc/cbmc", "goto-cc/goto-cc", 
+      bin.install "cbmc/cbmc", 
+                  "goto-cc/goto-cc", 
                   "goto-instrument/goto-instrument",
-                  "goto-analyzer/goto-analyzer"
+                  "goto-analyzer/goto-analyzer", 
+                  "goto-diff/goto-diff"
     end
   end
 
